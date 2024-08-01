efbb55c24beb8e6ef467ae30ba24fbb76ef45d40
OnePlustv <oneplustvpr0y1s@gmail.com>
2024-08-01 07:13:13 +0000

    variables

---
+++ chapterq/variable.js
@@ -0,0 +1,6 @@
+const accountId=202
+let accoutEmail="yqagmail.com"
+var accountPass="12344"
+accountCity="patna"
+console.log(accountId)
+console.table([accountId,accountPass,accoutEmail,accountCity])
