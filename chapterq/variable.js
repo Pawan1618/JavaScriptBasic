9e7c4369ba6cecdfec20577735e43755925978c2
OnePlustv <oneplustvpr0y1s@gmail.com>
2024-08-01 08:00:38 +0000

    comnt

--- chapterq/variable.js
+++ chapterq/variable.js
@@ -2,5 +2,12 @@ const accountId=202
 let accoutEmail="yqagmail.com"
 var accountPass="12344"
 accountCity="patna"
+let accountState;
+/*
+dont use var 
+also we can delcare variable without let,const,var
+use semicolon or not 
+cnnot declare undefine without datatype
+*/
 console.log(accountId)
-console.table([accountId,accountPass,accoutEmail,accountCity])
+console.table([accountId,accountPass,accoutEmail,accountCity, accountState])
