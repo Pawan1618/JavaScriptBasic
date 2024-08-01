03299f93901f82038b69cfc2ae117b47ed58eb46
Pawan1618 <148877397+Pawan1618@users.noreply.github.com>
2024-08-01 13:16:45 +0000

    new

---
+++ chapterq/conversion.js
@@ -0,0 +1,29 @@
+// let num=123
+// console.log(typeof(num))
+// let StringNumb=String(num)
+// // after converting num to string
+// console.log(typeof(StringNumb))
+// let string="33"
+// console.log(Number(string))
+// here we extract number from string
+// now focusing on operation
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2%2)
+console.log(2%3)
+console.log(2/3)
+*/
+// // checking string behabviour 
+// str="hello"
+// str2=" world"
+// console.log(str+ str2)
+// str3=1+"2"+"3"
+// str4="2"+2+4
+// str5=3+3+"4"
+// str6=4+4+"7"+7+7// intersting result once encounter string then treat all num as string afterwards
+// console.table([str3,str4,str5,str6])
+// work on operator precedence for knowledge only not for production code 
+// readability is preferd
