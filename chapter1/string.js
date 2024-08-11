7d17d2b9346d047503ceec7921d984cd471e0a72
Pawan1618 <148877397+Pawan1618@users.noreply.github.com>
2024-08-11 10:49:42 +0000

    few chsnge

--- chapter1/string.js
+++ chapter1/string.js
@@ -3,8 +3,9 @@ const dayPlayed="12days"
 console.log(GameName+ dayPlayed) // not readable enough should avoid
 // use string interpolation 
 
+console.log(`total ${GameName} is ${dayPlayed}`);
 
-//console.log('total ${GameName} is ${dayPlayed}');
+
 // new method to delcare string
 const newWay= new String('hitwsh code')
 console.log(newWay)
